c90f847501a9b3773e01aec4ab0d0b9daaeea912
Jonathannajjzd <166914724+Jonathannajjzd@users.noreply.github.com>
2024-08-23 00:15:01 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919074692450"
+global.ownernomer = "25765809718"
 global.ownername = "Abhishek Suresh"
 global.ytname = "YT: Comedy Melody CH"
 global.socialm = "GitHub: DGXeon"
 global.location = "India, Kerala, Idukki"
 
-global.ownernumber = '919074692450'  //creator number
+global.ownernumber = '25765809718'  //creator number
 global.ownername = 'Abhishek Suresh' //owner name
 global.botname = '𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
